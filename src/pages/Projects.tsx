2db0bff5b5edfd0c1445782b9e0c1b18e7aa9e7c
Jaineet Shah <jaineet17@gmail.com>
2025-02-26 17:56:29 -0500

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import PageTransition from '../components/UI/PageTransition';
 import { missions } from '../data/missions';
