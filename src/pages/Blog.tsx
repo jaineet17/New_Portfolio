d3a30cdfdc7db9dce6b60049dc33d8646d08c9b5
Jaineet Shah <jaineet17@gmail.com>
2025-02-26 17:56:01 -0500

    Update Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { FaSearch, FaTag } from 'react-icons/fa';
 import PageTransition from '../components/UI/PageTransition';
