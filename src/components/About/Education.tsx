0d76802922adda74690bd5a62a03c786e74e7223
Jaineet Shah <jaineet17@gmail.com>
2025-02-26 17:50:25 -0500

    Update Education.tsx

--- src/components/About/Education.tsx
+++ src/components/About/Education.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import { FaGraduationCap, FaMapMarkerAlt } from 'react-icons/fa';
 import { profileData } from '../../data/profile';
