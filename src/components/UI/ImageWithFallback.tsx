62b9352a72452bb310070846d43d1af3061a3369
Jaineet Shah <jaineet17@gmail.com>
2025-02-26 17:54:00 -0500

    Update ImageWithFallback.tsx

--- src/components/UI/ImageWithFallback.tsx
+++ src/components/UI/ImageWithFallback.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 interface ImageWithFallbackProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   fallbackSrc?: string;
