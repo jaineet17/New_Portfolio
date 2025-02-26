933e98b598fbffa2e65668135df3e596f773922f
Jaineet Shah <jaineet17@gmail.com>
2025-02-26 18:04:16 -0500

    Update FontLoader.ts

--- src/utils/FontLoader.ts
+++ src/utils/FontLoader.ts
@@ -1,16 +1,3 @@
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import { extend } from '@react-three/fiber';
-
-// Extend Three.js with TextGeometry
-extend({ TextGeometry });
-
-// Create font loader singleton
-const fontLoader = new FontLoader();
-
-// Load font and return promise
-export const loadFont = (url: string) => {
-  return new Promise((resolve, reject) => {
-    fontLoader.load(url, resolve, undefined, reject);
-  });
-}; 
